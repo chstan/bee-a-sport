561402ed89edd801e72625697887132b23b7294e
James Loewen <james.loewen@uwaterloo.ca>
2016-04-15 21:33:06 -0400

    Loading assets

--- src/game.js
+++ src/game.js
@@ -14,7 +14,30 @@ class Game {
         var $canvas = $('#game');
         var context = $canvas[0].getContext('2d');
         context.fillStyle = '#FF0000';
+        this.loadAssets();
     }
+
+    loadAssets() {
+        this.assets = {
+            bee: {
+                body: new Image,
+                leftWing: new Image,
+                rightWing: new Image,
+            }
+        };
+        this.assets.bee.body.src = '/assets/bee_body_draft.svg';
+        this.assets.bee.leftWing.src = '/assets/bee_wing_left_draft.svg';
+        this.assets.bee.rightWing.src = '/assets/bee_wing_right_draft.svg';
+        this.loadCount = 3;
+        this.assets.bee.body.onload = this.assetLoaded;
+        this.assets.bee.leftWing.onload = this.assetLoaded;
+        this.assets.bee.rightWing.onload = this.assetLoaded;
+    }
+
+    assetLoaded() {
+        this.loadCount -= 1;
+    }
+
 }
 
 $(document).ready(() => {
