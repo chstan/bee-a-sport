6dc75128dbc59deb336eee629d3c9d0af85a5f8c
Nik Klassen <me@nikklassen.ca>
2016-04-15 18:19:36 -0700

    Unify index and game

--- src/game.js
+++ src/game.js
@@ -1,3 +1,27 @@
-var $canvas = $("#game");
-var context = $canvas[0].getContext("2d");
-context.fillStyle "#FF0000";
+import io from 'socket.io-client';
+import $ from 'jquery';
+
+class Game {
+    constructor() {
+        this.socket = io.connect('http://localhost:8080');
+    }
+
+    register() {
+        this.socket.emit('register', 123);
+    }
+
+    start() {
+        var $canvas = $('#game');
+        var context = $canvas[0].getContext('2d');
+        context.fillStyle = '#FF0000';
+    }
+}
+
+$(document).ready(() => {
+    var game = new Game;
+    $('#register-button').click(() => {
+        game.register();
+    });
+
+    game.start();
+});
