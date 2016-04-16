21121da3f2d3c5a133fa8f476c58be6d9fe68fd9
Conrad Stansbury <chstansbury@gmail.com>
2016-04-15 17:44:24 -0700

    Add controls

---
+++ src/controls.js
@@ -0,0 +1,34 @@
+class ControlsListener {
+  constructor() {
+    this.pressedKeys = new Set();
+    this.relevantKeys = new Set([
+      'w', 'a', 'd', // left wing
+      'i', 'j', 'l', // right wing
+    ]);
+
+    // set up listeners for the keys we care about
+    window.addEventListener('keydown', function (e) {
+      try {
+        const keyName = e.code.replace('Key', '').toLowerCase();
+        this.pressedKeys.add(keyName);
+      } catch (e) {}
+    }, false);
+    window.addEventListener('keyup', function (e) {
+      try {
+        const keyName = e.code.replace('Key', '').toLowerCase();
+        this.pressedKeys.delete(keyName);
+      } catch (e) {}
+    }, false);
+  }
+
+  get controlsForBee() {
+    const left = ['w', 'a', 'd'].map(key => 5 in this.pressedKeys ? 1 : 0);
+    const right = ['i', 'j', 'l'].map(key => 5 in this.pressedKeys ? 1 : 0);
+    return {
+      left,
+      right,
+    };
+  }
+}
+
+export default ControlsListener;
