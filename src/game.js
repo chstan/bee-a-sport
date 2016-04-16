d192a57e017cdef8bcef7dbf6136ccb3dc668e0b
James Loewen <james.loewen@uwaterloo.ca>
2016-04-15 21:11:31 -0400

    Rendering the assets (probably wrong paths for actual app)

--- src/game.js
+++ src/game.js
@@ -1,3 +1,19 @@
-var $canvas = $("#game");
-var context = $canvas[0].getContext("2d");
-context.fillStyle "#FF0000";
+//import Bee from 'bee';
+
+var canvas = document.getElementById('game');
+var context = canvas.getContext("2d");
+var bodyImg = new Image;
+bodyImg.src = '../assets/bee_body_draft.svg';
+var leftWingImg = new Image;
+leftWingImg.src = '../assets/bee_wing_left_draft.svg';
+var rightWingImg = new Image;
+rightWingImg.src = '../assets/bee_wing_right_draft.svg';
+bodyImg.onload = function () {
+    context.drawImage(bodyImg, 300, 300);
+};
+leftWingImg.onload = function () {
+    context.drawImage(leftWingImg, 225, 350);
+};
+rightWingImg.onload = function () {
+    context.drawImage(rightWingImg, 450, 350);
+};
