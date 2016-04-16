586ba90769a1cf36518aaab2d12c099b52596780
Nik Klassen <me@nikklassen.ca>
2016-04-15 22:06:15 -0700

    Fix indentation

--- server.js
+++ server.js
@@ -11,47 +11,47 @@ app.use('/static', express.static(__dirname + '/static'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
 var games = {},
-    keys = {};
+  keys = {};
 
-io.sockets.on('connection', socket => {
+  io.sockets.on('connection', socket => {
     socket.on('register', data => {
-        console.log(`Received registration ${JSON.stringify(data)}`);
-        switch (_.size(games[data.gamekey])) {
-            case 0:
-                games[data.gamekey] = [];
-                /* FALL-THROUGH */
-            case 1:
-                games[data.gamekey].push({
-                    user: data.name,
-                    socket,
-                });
-                keys[socket] = data.gamekey;
-                break;
-            default:
-                console.log(`Game already full for key ${data.gamekey}`)
-                return;
-        }
-        if (games[data.gamekey].length === 2) {
-            games[data.gamekey].forEach(u => {
-                u.socket.emit('start');
-            });
-        }
+      console.log(`Received registration ${JSON.stringify(data)}`);
+      switch (_.size(games[data.gamekey])) {
+          case 0:
+              games[data.gamekey] = [];
+              /* FALL-THROUGH */
+          case 1:
+              games[data.gamekey].push({
+                user: data.name,
+                socket,
+              });
+              keys[socket] = data.gamekey;
+              break;
+          default:
+              console.log(`Game already full for key ${data.gamekey}`)
+              return;
+      }
+      if (games[data.gamekey].length === 2) {
+        games[data.gamekey].forEach(u => {
+          u.socket.emit('start');
+        });
+      }
     });
 
     socket.on('reset', () => {
-        console.log('Reseting all games');
-        io.emit('reset');
-        games = {};
-        keys = {};
+      console.log('Reseting all games');
+      io.emit('reset');
+      games = {};
+      keys = {};
     });
 
     socket.on('state-update', beeState => {
-        const gamekey = keys[socket];
-        if (!gamekey || games[gamekey].length < 2) return;
+      const gamekey = keys[socket];
+      if (!gamekey || games[gamekey].length < 2) return;
 
-        const opponent = _.filter(games[gamekey], g => g.socket !== socket)[0];
-        opponent.socket.emit('state-update', beeState);
+      const opponent = _.filter(games[gamekey], g => g.socket !== socket)[0];
+      opponent.socket.emit('state-update', beeState);
     });
-});
+  });
 
-server.listen(runningPortNumber);
+  server.listen(runningPortNumber);
